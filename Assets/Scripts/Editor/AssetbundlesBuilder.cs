9de689f26a4f78be5df6168b3909c0591ac55509
Terrier Li <carronailo@gmail.com>
2017-11-08 12:34:47 +0800

    1

--- Assets/Scripts/Editor/AssetbundlesBuilder.cs
+++ Assets/Scripts/Editor/AssetbundlesBuilder.cs
@@ -16,30 +16,41 @@ public class AssetbundlesBuilder
 	[MenuItem("My Tools/Build All Assetbundles (Default)", priority = 1)]
 	public static void BuildAllAssetbundles()
 	{
-		// 确保路径存在
-		IOUtil.MakeSurePath(EditorConstants.ASSETBUNDLE_ABSOLUTE_PATH);
-#if UNITY_STANDALONE_WIN
 		AssetBundleManifest manifest = null;
+		string outputPath = EditorConstants.ASSETBUNDLE_RELATIVE_PATH;
+#if UNITY_STANDALONE_WIN
 #if USE_64BIT
-		manifest = BuildPipeline.BuildAssetBundles(EditorConstants.ASSETBUNDLE_RELATIVE_PATH, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+		outputPath += BuildTarget.StandaloneWindows64 + "/";
+		// 确保路径存在
+		IOUtil.MakeSurePath(outputPath);
+		manifest = BuildPipeline.BuildAssetBundles(outputPath, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
 #else
-		manifest = BuildPipeline.BuildAssetBundles(EditorConstants.ASSETBUNDLE_RELATIVE_PATH, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+		outputPath += BuildTarget.StandaloneWindows + "/";
+		// 确保路径存在
+		IOUtil.MakeSurePath(outputPath);
+		manifest = BuildPipeline.BuildAssetBundles(outputPath, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+#endif
+#elif UNITY_ANDROID
+		outputPath += BuildTarget.Android + "/";
+		// 确保路径存在
+		IOUtil.MakeSurePath(outputPath);
+		manifest = BuildPipeline.BuildAssetBundles(outputPath, BuildAssetBundleOptions.None, BuildTarget.Android);
+#elif UNITY_IOS
+		outputPath += BuildTarget.iPhone + "/";
+		// 确保路径存在
+		IOUtil.MakeSurePath(outputPath);
+		manifest = BuildPipeline.BuildAssetBundles(outputPath, BuildAssetBundleOptions.None, BuildTarget.iPhone);
 #endif
 		string[] assetbundles = manifest.GetAllAssetBundles();
 		StringBuilder sb = new StringBuilder();
-		foreach(string assetbundle in assetbundles)
+		foreach (string assetbundle in assetbundles)
 		{
 			sb.AppendLine(manifest.GetAssetBundleHash(assetbundle).ToString());
 		}
 		LogConsole.Log(Tag.Builder, sb.ToString());
 		uint crc;
-		BuildPipeline.GetCRCForAssetBundle(EditorConstants.ASSETBUNDLE_RELATIVE_PATH + "joystick", out crc);
+		BuildPipeline.GetCRCForAssetBundle(outputPath + "joystick", out crc);
 		LogConsole.Log(Tag.Builder, crc);
-#elif UNITY_ANDROID
-
-#elif UNITY_IOS
-
-#endif
 	}
 
 	[MenuItem("My Tools/Get All Assetbundles Names", priority = 2)]
